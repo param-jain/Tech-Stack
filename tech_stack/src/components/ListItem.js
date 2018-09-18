056b4b8a2ac898bfbce5e3f1be53844138d6b1b2
Param Jain <param_jain@icloud.com>
2018-09-18 09:25:09 +0530

    Rendering Lists using Redux I
    Learnt and used ListView, FlatView.
    Syling of List.
    Introduction to ActionCreators.
    Calling ActionCreators.

---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+
+  render() {
+    const { titleStyle } = styles;
+    console.log(this.props);
+
+    return (
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {this.props.library.item.title}
+            </Text>
+          </CardSection>
+        </View>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  },
+};
+
+export default connect(null, actions)(ListItem);
